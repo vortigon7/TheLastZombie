f2e60122c536bbdcc248ab3bdc3401c9caffd4a8
vortigon7 <andras.benedek.horvath@gmail.com>
2018-10-02 19:03:03 +0200

    Initial commit

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform player;
+	private float smoothSpeed = 8f;
+	public Vector3 offset;
+
+	private Vector3 mousePosition;
+
+
+	void LateUpdate ()
+	{
+		Vector3 mousePositionRaw = Input.mousePosition;
+		mousePosition = Camera.main.ScreenToWorldPoint (mousePositionRaw);
+		Vector3 directionalOffset = new Vector3 (mousePosition.x - transform.position.x, mousePosition.y - transform.position.y, mousePosition.z - transform.position.z);
+		if (Mathf.Abs(directionalOffset.x) > 3f) {
+			if (directionalOffset.x >= 0) {
+				directionalOffset.x = 3f;
+			}
+			if (directionalOffset.x < 0) {
+				directionalOffset.x = -2f;
+			}
+		}
+		if (Mathf.Abs(directionalOffset.y) > 3f) {
+			if (directionalOffset.y >= 0) {
+				directionalOffset.y = 3f;
+			}
+			if (directionalOffset.y < 0) {
+				directionalOffset.y = -3f;
+			}
+		}
+		if (Mathf.Abs(directionalOffset.z) > 3f) {
+			if (directionalOffset.z >= 0) {
+				directionalOffset.z = 3f;
+			}
+			if (directionalOffset.z < 0) {
+				directionalOffset.z = -3f;
+			}
+		}
+		Vector3 desiredPosition = player.position + offset + directionalOffset;
+		Vector3 smoothPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+		transform.position = smoothPosition;
+
+	}
+
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float playerSpeed = 10f;
+	public float dashTimer;
+	public float dashDelay;
+	//private int dashDirection;
+	private Rigidbody2D playerRigidBody;
+	public GameObject player;
+	private Vector2 spriteDirection;
+	private Vector2 playerDirection;
+
+	void Start () {
+		playerRigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		/*if (Input.GetKey (KeyCode.W)) {
+			Move (Vector2.up);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			Move (Vector2.down);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			Move (Vector2.left);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			Move (Vector2.right);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Mouse1)) {
+			Dash ();
+		}
+		if (dashTimer >= 0) {
+			dashTimer -= Time.deltaTime;
+		}*/
+
+		FaceMouse ();
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		playerRigidBody.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal")* playerSpeed, 0.99f), Mathf.Lerp(0, Input.GetAxis("Vertical")* playerSpeed, 0.99f));
+	}
+
+	/*void Move (Vector2 moveDirection) {
+		Vector2 playerDirection = moveDirection;
+		if (playerDirection.sqrMagnitude > 1) {
+			player.transform.Translate (playerDirection * playerSpeed * Mathf.Sqrt(2) * Time.deltaTime);
+		} else {
+			player.transform.Translate (playerDirection * playerSpeed * Time.deltaTime);
+		}
+	}*/
+
+	void FaceMouse () {
+		Vector2 mousePosition = Input.mousePosition;
+		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
+
+		spriteDirection = new Vector2 (mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+		transform.up = spriteDirection;
+	}
+
+	void MachineGun () {
+
+	}
+
+	void Claw () {
+
+	}
+
+	void OverloadGun () {
+
+	}
+
+	void Dash () {
+		Vector3 dashDirection = new Vector3 (spriteDirection.x, spriteDirection.y, 0);
+
+		if (dashTimer <= 0) {
+			//player.transform.Translate (dashDirection.normalized * 1000f * Time.deltaTime);
+			//player.transform.position = Vector2.Lerp (player.transform.position, player.transform.position + dashDirection.normalized * 1000f * Time.deltaTime, Time.deltaTime);
+			//playerRigidBody.AddForce (transform.up * 5f);
+			dashTimer = dashDelay;
+		}
+	}
+}
