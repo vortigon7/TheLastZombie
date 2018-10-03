0dd680d7679cead65a36339b5dbfb40222eb8774
G-a-b-e <43213651+G-a-b-e@users.noreply.github.com>
2018-10-03 07:31:36 +0200

    Bases of movement
    Addforce based movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,58 +5,52 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour {
 
 	// Public variables used in controlling the player 
-	public float playerSpeed = 10f;
+	public float playerSpeed = 3;
+	public float maxSpeed = 20;
 	public float dashTimer;
 	public float dashDelay;
+	public float MouseAngle;
+
 	//private int dashDirection;
 	private Rigidbody2D playerRigidBody;
 	public GameObject player;
 	private Vector2 spriteDirection;
 	private Vector2 playerDirection;
+	private Vector2 DashMovement;
 
 	void Start () {
 		playerRigidBody = GetComponent<Rigidbody2D> ();
+
 	}
 
 	// Update is called once per frame
 	void Update () {
-		/*if (Input.GetKey (KeyCode.W)) {
-			Move (Vector2.up);
-		}
-		if (Input.GetKey (KeyCode.S)) {
-			Move (Vector2.down);
-		}
-		if (Input.GetKey (KeyCode.A)) {
-			Move (Vector2.left);
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			Move (Vector2.right);
+
+		if (dashTimer >= -1) {
+			dashTimer -= Time.deltaTime;
 		}
 
-		if (Input.GetKeyDown (KeyCode.Mouse1)) {
-			Dash ();
-		}
-		if (dashTimer >= 0) {
-			dashTimer -= Time.deltaTime;
-		}*/
 
 		FaceMouse ();
 	}
 
-	/*void Move (Vector2 moveDirection) {
-		Vector2 playerDirection = moveDirection;
-		if (playerDirection.sqrMagnitude > 1) {
-			player.transform.Translate (playerDirection * playerSpeed * Mathf.Sqrt(2) * Time.deltaTime);
-		} else {
-			player.transform.Translate (playerDirection * playerSpeed * Time.deltaTime);
-		}
-	}*/
-
-	void FixedUpdate () {
+	void FixedUpdate ()
+	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		playerRigidBody.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal")* playerSpeed, 0.99f), Mathf.Lerp(0, Input.GetAxis("Vertical")* playerSpeed, 0.99f));
+		float accelerationMultiplier = 1 - (playerRigidBody.velocity.magnitude / maxSpeed);
+		Vector2 ZeroVector = new Vector2 (0f, 0f);
+		Vector2 MouseCircle = (new Vector2 (Mathf.Sin (MouseAngle), Mathf.Cos (MouseAngle)) * 15.3f);
+		Vector2 Movement = new Vector2(Input.GetAxis("Horizontal")* playerSpeed * accelerationMultiplier * Time.deltaTime, Input.GetAxis("Vertical") * playerSpeed * accelerationMultiplier * Time.deltaTime);
+		playerRigidBody.AddForce (Movement);
+
+		if ((Input.GetKeyDown (KeyCode.Mouse1)) && (dashTimer <= 0)) {
+			playerRigidBody.AddForce (MouseCircle * 1f);
+			dashTimer = dashDelay;
+			Debug.Log (playerRigidBody);
+		}
+			
+
 	}
 
 	void FaceMouse () {
@@ -64,29 +58,14 @@ public class PlayerControl : MonoBehaviour {
 		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
 
 		spriteDirection = new Vector2 (mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+		MouseAngle = Mathf.Atan2 (spriteDirection.x, spriteDirection.y);
+		 
 		transform.up = spriteDirection;
 	}
-
-	void MachineGun () {
-
-	}
-
-	void Claw () {
-
-	}
-
-	void OverloadGun () {
-
-	}
-
+		
 	void Dash () {
 		Vector3 dashDirection = new Vector3 (spriteDirection.x, spriteDirection.y, 0);
+			
 
-		if (dashTimer <= 0) {
-			//player.transform.Translate (dashDirection.normalized * 1000f * Time.deltaTime);
-			//player.transform.position = Vector2.Lerp (player.transform.position, player.transform.position + dashDirection.normalized * 1000f * Time.deltaTime, Time.deltaTime);
-			//playerRigidBody.AddForce (transform.up * 5f);
-			dashTimer = dashDelay;
-		}
 	}
 }
