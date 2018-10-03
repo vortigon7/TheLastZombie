a11e00c31dbdf7d6478d66c42647747b27713c74
vortigon7 <andras.benedek.horvath@gmail.com>
2018-10-03 08:48:08 +0200

    Mooore fine-tuning
    - Renamed a lot of variables.
    - Fine-tuned speed/linear-drag relation.
    - Created dash state.
    - Created deceleration when under certain speed.
    
    - Note: the collision is sometimes buggy, needs work.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour {
 
 	// Public variables used in controlling the player 
-	public float playerSpeed = 50f;
+	public float pSpeed = 50f;
 	public float maxSpeed = 50f;
 	public float dashTimer;
 	public float dashDelay;
-	public float MouseAngle;
+	public float mouseAngle;
 
 	//private int dashDirection;
-	private Rigidbody2D playerRigidBody;
+	private Rigidbody2D pRigidBody;
 	public GameObject player;
 	private Vector2 spriteDirection;
-	private Vector2 playerDirection;
-	private Vector2 DashMovement;
-	private Vector2 velocityOld;
+	private Vector2 pDirection;
+	private Vector2 pVelocityOld;
+	private Vector2 pMovement;
+	private bool pDashState;
 
 	void Start () {
-		playerRigidBody = GetComponent<Rigidbody2D> ();
+		pRigidBody = GetComponent<Rigidbody2D> ();
 
 	}
 
@@ -34,30 +35,39 @@ public class PlayerControl : MonoBehaviour {
 		FaceMouse ();
 		int n = 0;
 		while (n < (3 * Time.deltaTime)) {
-			Debug.Log (playerRigidBody.velocity.magnitude);
+			Debug.Log (pRigidBody.velocity.magnitude);
 			n++;
 		}
 
-		velocityOld = playerRigidBody.velocity;
+		pVelocityOld = pRigidBody.velocity;
 	}
 
 	void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		float accelerationMultiplier = 1 - (playerRigidBody.velocity.magnitude / maxSpeed);
-		Vector2 MouseCircle = (new Vector2 (Mathf.Sin (MouseAngle), Mathf.Cos (MouseAngle)) * playerSpeed);
-		Vector2 Movement = new Vector2(moveHorizontal * playerSpeed * accelerationMultiplier * Time.deltaTime, moveVertical * playerSpeed * accelerationMultiplier * Time.deltaTime);
-		playerRigidBody.AddForce (Movement);
+		float accMulti = 1 - (pRigidBody.velocity.magnitude / maxSpeed);
+		//Vector2 mouseCircle = (new Vector2 (Mathf.Sin (mouseAngle), Mathf.Cos (mouseAngle)) * pSpeed);
+		if (pDashState) {
+			pMovement = Vector2.zero;
+			if (pRigidBody.velocity.magnitude < 5) {
+				pDashState = false;
+			}
+		} else {
+			pMovement = new Vector2 (moveHorizontal * pSpeed * accMulti * Time.deltaTime, moveVertical * pSpeed * accMulti * Time.deltaTime);
+		}
+		pRigidBody.AddForce (pMovement);
 
-		if ((playerRigidBody.velocity.magnitude < 1) && (velocityOld.sqrMagnitude > playerRigidBody.velocity.sqrMagnitude)) {
-			playerRigidBody.velocity -= playerRigidBody.velocity * Time.deltaTime * 300f;
+		if ((pRigidBody.velocity.magnitude < 1) && (pVelocityOld.sqrMagnitude > pRigidBody.velocity.sqrMagnitude)) {
+			pRigidBody.velocity -= pRigidBody.velocity * Time.deltaTime * 300f;
 		}
 
 		if ((Input.GetKeyDown (KeyCode.Mouse1)) && (dashTimer <= 0)) {
-			playerRigidBody.AddForce (MouseCircle * 1f);
+			//pRigidBody.velocity = Vector2.zero;
+			pDashState = true;
+			pRigidBody.AddForce (spriteDirection.normalized * pSpeed * 1f);
 			dashTimer = dashDelay;
-			Debug.Log (playerRigidBody);
+			//Debug.Log (pRigidBody);
 		}
 			
 
@@ -68,7 +78,7 @@ public class PlayerControl : MonoBehaviour {
 		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
 
 		spriteDirection = new Vector2 (mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
-		MouseAngle = Mathf.Atan2 (spriteDirection.x, spriteDirection.y);
+		mouseAngle = Mathf.Atan2 (spriteDirection.x, spriteDirection.y);
 		 
 		transform.up = spriteDirection;
 	}
